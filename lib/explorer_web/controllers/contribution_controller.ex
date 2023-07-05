2505b0db883dcd42e8c7a4e9ede3aab2a7d8aea4
Fernando Mendes <fernando@mendes.codes>
2023-07-05 02:38:26 +0100

    Remove lingering IO.inspect/1

--- lib/explorer_web/controllers/contribution_controller.ex
+++ lib/explorer_web/controllers/contribution_controller.ex
@@ -7,7 +7,7 @@ defmodule ExplorerWeb.ContributionController do
 
   def index(conn, params) do
     with {:ok, contributions} <-
-           FetchContributions.perform(params["eth_address"]) |> IO.inspect(label: "fetch") do
+           FetchContributions.perform(params["eth_address"]) do
       render(conn, :index, contributions: contributions)
     end
   end
