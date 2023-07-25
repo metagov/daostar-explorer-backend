49151b89ecbcdf9af26e01a7d98986501d64ae7f
Fernando Mendes <fernando@mendes.codes>
2023-07-25 08:57:47 +0100

    Add seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,11 +1,12 @@
-# Script for populating the database. You can run it as:
-#
-#     mix run priv/repo/seeds.exs
-#
-# Inside the script, you can read and write to any of your
-# repositories directly:
-#
-#     Explorer.Repo.insert!(%Explorer.SomeSchema{})
-#
-# We recommend using the bang functions (`insert!`, `update!`
-# and so on) as they will fail if something goes wrong.
+users = %{
+  "0xb7a000c543aC7E39fDf4fC391B3900078E070325" => 11,
+  "0x1eF94BEa1542da6d61c68f26f7a3933A240C87bD" => 12,
+  "0x710DD7f6e3e47Ddc08fac4795694d7ec4506C4e6" => 13
+}
+
+for {eth_address, seller_id} <- users do
+  Explorer.Accounts.create_user(%{
+    eth_address: eth_address,
+    reputable_seller_id: seller_id
+  })
+end
