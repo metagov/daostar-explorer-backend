d349c02d692a9aebe5acfc2e0cb17f54ff1f725a
Fernando Mendes <fernando@mendes.codes>
2023-08-07 20:16:30 +0100

    Remove unnecessary field from DAO json

--- lib/explorer_web/controllers/dao_json.ex
+++ lib/explorer_web/controllers/dao_json.ex
@@ -4,9 +4,7 @@ defmodule ExplorerWeb.DAOJSON do
   def show(%{dao: dao}) do
     %{
       "@context": "https://daostar.org/schemas",
-      type: "DAO",
       name: dao.name,
-      description: dao.description,
       attestationIssuerURIs: AttestationIssuerJSON.index(dao)
     }
   end
